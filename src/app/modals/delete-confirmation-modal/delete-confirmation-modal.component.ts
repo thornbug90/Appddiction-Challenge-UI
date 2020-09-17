d0fbe6aef96c6d99e813fe8a837153df08280ec0
thornbug90 <crazydev0409@gmail.com>
2020-09-17 16:01:10 -0500

    added dependencies

--- src/app/modals/delete-confirmation-modal/delete-confirmation-modal.component.ts
+++ src/app/modals/delete-confirmation-modal/delete-confirmation-modal.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { PersonService } from 'src/app/services/person.service';
-import { timeStamp } from 'console';
+// import { timeStamp } from 'console';
 
 @Component({
   selector: 'app-delete-confirmation-modal',
@@ -17,20 +17,21 @@ export class DeleteConfirmationModalComponent implements OnInit {
     private dialogRef: MatDialogRef<DeleteConfirmationModalComponent>,
     private dialog: MatDialog,
     @Inject(MAT_DIALOG_DATA) public data: any
-  ) { 
+  ) {
     this.id = data.value.toString();
   }
 
   ngOnInit(): void {
   }
 
+
   onConfirmDelete() {
-  
+
     this.ps.deleteEntry(this.id).toPromise().then(res => {
-      
+
     });
     this.dialogRef.close();
     // this.snotify.success('Contact deleted :)');
-  } 
+  }
 
 }
